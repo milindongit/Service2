8dfcf0c57d84a26d6712cf22c34ccee48e003935
Geet Space <milinde@rediffmail.com>
2021-05-31 23:17:10 +0530

    Added WeatherForecastControllerTests.cs added

--- ItemService.UnitTests.MSBuild/WeatherForecastControllerTests.cs
+++ ItemService.UnitTests.MSBuild/WeatherForecastControllerTests.cs
@@ -1,14 +1,20 @@
+using ItemService.Controllers;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ItemService.UnitTests.MSBuild
 {
     public class WeatherForecastControllerTests
     {
+        private readonly static ILogger<WeatherForecastController> _logger;
+        private readonly WeatherForecastController weatherForecastController = new WeatherForecastController(_logger);
         [Fact]
-        public void Test1()
+        public void WeatherForecastController_Get_Returns_WeatherSummary()
         {
-
+            IEnumerable<WeatherForecast> listOfWeatherForecasts = weatherForecastController.Get();
+            Xunit.Assert.NotNull(listOfWeatherForecasts);
         }
     }
 }
